61eed020adac26ead5734f3b7c1be0f3bff13626
jakub <jakub1994@gmail.com>
2022-12-10 23:44:30 +0100

    Added way to load secrets and .gitignore

--- replicateAPI.py
+++ replicateAPI.py
@@ -1,13 +1,27 @@
 import replicate
 
-def callDeforumOnReplicate(max_frames=100, prompt="a beautiful portrait of a woman by Artgerm, trending on Artstation",
+def loadSecrets():
+    secrets = {}
+    with open ("secrets.txt", 'r') as f:
+         for line in f:
+            key, value = line.strip().split('=')
+            secrets[key] = value
+    return secrets
+
+def initializeReplicateClient(secrets):
+    client = replicate.Client(api_token=secrets["REPLICATE_API_TOKEN"])
+    return client
+
+def callDeforumOnReplicate(client, max_frames=100, prompt="a beautiful portrait of a woman by Artgerm, trending on Artstation",
 angle="0:(0)", zoom="0: (1.04)", translation_x="0: (0)", translation_y="0: (0)", color_coherence="Match Frame 0 LAB",
 sampler="plms", fps=15, seed=None):
-    model = replicate.models.get("deforum/deforum_stable_diffusion")
+    model = client.models.get("deforum/deforum_stable_diffusion")
     version = model.versions.get("e22e77495f2fb83c34d5fae2ad8ab63c0a87b6b573b6208e1535b23b89ea66d6")
     output = version.predict(max_frames, prompt, angle, zoom, translation_x, translation_y,
     color_coherence, sampler, fps, seed)
     return output
 
 if __name__ == "__main__":
-    print(callDeforumOnReplicate())
+    secrets = loadSecrets()
+    client = initializeReplicateClient(secrets)
+    print(callDeforumOnReplicate(client))
