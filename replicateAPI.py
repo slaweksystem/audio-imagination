d0698823e429eb0589afb71286c7192e306e271b
jakub <jakub1994@gmail.com>
2022-12-11 18:12:08 +0100

    Replicate times out at 1000 frames, so decreasing to 850

--- replicateAPI.py
+++ replicateAPI.py
@@ -28,8 +28,8 @@ class Replicate:
     def callDeforumOnReplicate(self, max_frames=100, prompt="a beautiful portrait of a woman by Artgerm, trending on Artstation",
     angle="0:(0)", zoom="0: (1.04)", translation_x="0: (0)", translation_y="0: (0)", color_coherence="Match Frame 0 LAB",
     sampler="plms", fps=15, seed=None):
-        max_frames = max(max_frames, 1000)
-        
+        if max_frames > 850:
+            max_frames = 850
         model = self.client.models.get("deforum/deforum_stable_diffusion")
         version = model.versions.get("e22e77495f2fb83c34d5fae2ad8ab63c0a87b6b573b6208e1535b23b89ea66d6")
         input = {
