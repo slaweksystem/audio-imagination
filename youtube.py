cae267fd877c40f3f22bbc4e0396669d9d008cce
slaweksystem <slaweksystem@gmail.com>
2022-12-11 01:04:04 +0100

    youtube download

---
+++ youtube.py
@@ -0,0 +1,22 @@
+from pytube import YouTube
+import moviepy.editor as mp
+
+def youtube_download(url):
+    # creating YouTube object
+    yt = YouTube(url) 
+
+    # accessing audio streams of YouTube obj.(first one, more available)
+    stream = yt.streams.filter().first()
+    # downloading a video would be: stream = yt.streams.first() 
+
+    # download into working directory
+    path = stream.download(output_path='video.mp4')
+
+    # converting to mp3
+    clip = mp.VideoFileClip(path)
+    clip.audio.write_audiofile('audio.mp3')
+    # Getting some basic information
+    info = {}
+    info['title'] = yt.title
+    info['keywords'] = yt.keywords
+    return info
